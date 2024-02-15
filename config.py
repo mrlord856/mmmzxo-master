50f50e57734441e0359e1236830b6d63d8ca1f14
mrlord856 <151881197+mrlord856@users.noreply.github.com>
2024-02-15 13:20:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mrlord856/mmmr.git",
+    "https://github.com/mrlord856/mmmzxo-master.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
